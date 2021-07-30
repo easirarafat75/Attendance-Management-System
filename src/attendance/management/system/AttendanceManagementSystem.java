aee2501bb6b1bd5e3a4f620a427582cf97eff23f
easirarafat75 <easirarafat75@outlook.com>
2021-07-30 23:03:56 +0600

    adding project to github

---
+++ src/attendance/management/system/AttendanceManagementSystem.java
@@ -0,0 +1,14 @@
+
+package attendance.management.system;
+
+
+public class AttendanceManagementSystem {
+
+    
+    public static void main(String[] args) {
+        selection fp = new  selection ();
+        fp.setVisible(true);
+        fp.setLocationRelativeTo(null);
+    }
+    
+}
